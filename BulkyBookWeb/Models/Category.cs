c28e1af2c3cd931889ecd77e49feda02c7172aad
Bruno Lazzara Queiroz <lazzara.bruno@gmail.com>
2022-04-21 16:15:07 -0300

    New connection string and publishing
    DB published through GearHost

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -13,7 +13,6 @@ public class Category
     public string Name { get; set; }
 
     [DisplayName("Display Order")]
-    [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100 only!")]
     public int DisplayOrder { get; set; }
 
     public DateTime CreatedDateTime { get; set; } = DateTime.Now;
